ad03d4686eb03d6134ba6793a252409188ba1447
Loffes <43013032+Loffes@users.noreply.github.com>
2018-09-07 19:40:47 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.Payment					  = 10
+Config.TimeToPackBox			  = 20 -- Sekunder
+
+Config.Zones = {
+
+  StartJob = {
+    Pos   = { x = 1021.81, y = -3111.46, z = -39.85 },
+    Size  = { x = 2.0, y = 2.0, z = 1.0 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 27,
+  },
+  
+    LeaveBox = {
+    Pos   = { x = 1000.82, y = -3093.68, z = -39.85 },
+    Size  = { x = 2.0, y = 2.0, z = 1.0 },
+    Color = { r = 255, g = 0, b = 0 },  
+    Type  = 27,
+  },
+  
+    TeleportIn = {
+    Pos   = { x = 1800.69, y = 2483.15, z = -123.54 },
+    Size  = { x = 1.0, y = 1.0, z = 1.0 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 27,
+  },
+  
+    TeleportUt = {
+    Pos   = { x = 993.04, y = -3097.93, z = -39.85 },
+    Size  = { x = 1.0, y = 1.0, z = 1.0 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 27,
+  },
+}
